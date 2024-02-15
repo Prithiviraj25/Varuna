be4b1542a12e37f27efd72b7c34a11ab2998ef5e
Prithiviraj N <prithivi@Prithivirajs-MacBook-Pro.local>
2024-02-16 01:25:56 +0530

    update commit

--- lib/Intro_page.dart
+++ lib/Intro_page.dart
@@ -14,48 +14,50 @@ class IntroPage extends StatelessWidget {
                       textStyle: TextStyle(color: Color(0xDD000000)),
                       fontWeight: FontWeight.w800))),
         ),
-        body:
-          Container(
-            height: double.infinity,
-            width: double.infinity,
-            color: Color(0xFFE4F1FF),
-            child: Column(
-              children: [
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(children: [
-                    Text("Varuna is a real time water quality\n",
-                        style: GoogleFonts.roboto(
-                          fontSize: 20,
-                          textStyle: TextStyle(color: Color(0xDD000000)),
-                          fontWeight: FontWeight.w800,
-                        ))
-                  ]),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                InkWell(
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => MyHomePage()));
-                  },
-                  onLongPress: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => MyHomePage()));
-                  },
-                  onDoubleTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => MyHomePage()));
-                  },
-                  child: Text("CONTINUE",
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          color: Color(0xFFE4F1FF),
+          child: Column(
+            children: [
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(children: [
+                  Text(
+                      "Varuna is a real time water quality\nmonitoring app that enables us to\nmonitor the watersource that we\n"
+                      "consume water from.\n"
+                      "The app measures various values \nlike pH, solids, sulfates and many more\nand provides us with the information on\n"
+                      "the potability of the source that water is \nconsumed from.\n\nPlease refer to our youtube video for   \nmore information\nwww.youtube.com\n",
                       style: GoogleFonts.roboto(
-                          textStyle: TextStyle(color: Color(0xDD000000)),
-                          fontWeight: FontWeight.w800)),
-                )
-              ],
-            ),
-          )
-        );
+                        fontSize: 20,
+                        textStyle: TextStyle(color: Color(0xDD000000)),
+                        fontWeight: FontWeight.w800,
+                      ))
+                ]),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              InkWell(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MyHomePage()));
+                },
+                onLongPress: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MyHomePage()));
+                },
+                onDoubleTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MyHomePage()));
+                },
+                child: Text("CONTINUE",
+                    style: GoogleFonts.roboto(
+                        textStyle: TextStyle(color: Color(0xDD000000)),
+                        fontWeight: FontWeight.w800)),
+              )
+            ],
+          ),
+        ));
   }
 }
